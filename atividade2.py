9bb252c330e2ca2033d485c3639fdba693c18215
RangelMRK <marcosvrangel@hotmail.com>
2025-05-21 20:33:49 -0300

    Adicionada Atividade 3: Gorjeta

--- atividade2.py
+++ atividade2.py
@@ -1,5 +1,8 @@
 # EXERCICIO PRATICO 2
-# Crie um programa que permita a um professor registrar as notas de uma turma. O programa deve continuar solicitando notas até que o professor digite 'fim'. Notas válidas são de 0 a 10. O programa deve ignorar notas inválidas e continuar solicitando. No final, deve exibir a média da turma.
+# Crie um programa que permita a um professor registrar as notas de uma turma. O programa deve continuar solicitando notas até que o professor digite 'fim'.
+# Notas válidas são de 0 a 10. 
+# O programa deve ignorar notas inválidas e continuar solicitando. 
+# No final, deve exibir a média da turma.
 
 def registro():
     notas = []
---
+++ atvidadeGorjeta.py
@@ -0,0 +1,22 @@
+# Crie uma função que calcule a gorjeta a ser deixada em um restaurante baseada no valor total da conta e na porcentagem de gorjeta desejada.
+# Calcula o valor da gorjeta baseado no total da conta e na porcentagem desejada.
+
+# Parâmetros:
+# valor_conta(float): O valor total da conta
+# porcentagem_gorjeta(float): A porcentagem da gorjeta (Ex: 15 para 15%)
+
+# Retorna:
+# float: O valor da gorjeta calculada
+
+
+def gorjeta():
+    print('Digite o valor total da conta')
+    valor_conta = float(input())
+    print('Digite a porcentagem que gostaria de dar de gorjeta')
+    porcentagem_gorjeta = float(input())
+
+    gorjeta = valor_conta * (porcentagem_gorjeta/100)
+
+    print('O valor da gorjeta desta conta é de R$', gorjeta)
+
+gorjeta()
